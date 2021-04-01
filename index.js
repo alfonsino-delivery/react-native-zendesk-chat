5d9caad6b51b14fedf3beb8a2359575af3d97b7d
Mirko <ricci.mirko94@gmail.com>
2021-04-01 15:54:14 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import { NativeModules } from "react-native";
+
+const RNZendeskChatModule = NativeModules.RNZendeskChatModule;
+
+/**
+ * TypeScript Documentation for this Module describes the available methods & parameters
+ *
+ * @see { ./RNZendeskChat.d.ts }
+ */
+export default RNZendeskChatModule;
